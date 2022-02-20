1fc467657b3760179f9748336a49a97e9d20ea81
abderrahimFizazi <85798731+abderrahimFizazi@users.noreply.github.com>
2022-02-20 16:07:37 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Dashboard from './components/admin/Dashboard'
 import "./App.css"
 import Login from './components/admin/Login'
